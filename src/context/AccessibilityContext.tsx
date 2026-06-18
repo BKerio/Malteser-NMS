443b34bdceaf00fec5e82a5274f77bee6e1483b4
BKerio <briankerio48@gmail.com>
2026-06-18 22:15:06 +0300

    new updates

--- src/context/AccessibilityContext.tsx
+++ src/context/AccessibilityContext.tsx
@@ -115,7 +115,7 @@ export function AccessibilityProvider({
       setFontScale,
       setFontWeight,
       setDyslexiaFont,
-      resetAcceessibility,
+      resetAccessibility,
       getTextStyle,
       fontsLoaded,
     }),
