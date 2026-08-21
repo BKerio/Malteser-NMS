f620cdfa76083a5710c16db865e47ba827e9f0a7
BKerio <briankerio48@gmail.com>
2026-08-21 23:11:27 +0300

    new updates for operaters

--- src/components/navigation/DrawerContent.tsx
+++ src/components/navigation/DrawerContent.tsx
@@ -186,7 +186,7 @@ const styles = StyleSheet.create({
   },
   footerItem: {
     flexDirection: 'row',
-    alignItems: 'center',kk
+    alignItems: 'center',
     gap: 14,
     paddingVertical: 14,
     paddingHorizontal: 12,
