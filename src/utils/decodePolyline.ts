715fcb58ae93f4ef6515beaf30bcc5faea72ffa0
BKerio <briankerio48@gmail.com>
2026-08-21 23:04:36 +0300

    new updates for operaters

--- src/utils/decodePolyline.ts
+++ src/utils/decodePolyline.ts
@@ -3,7 +3,7 @@ export function decodePolyline(encoded: string): Array<{ latitude: number; longi
   const points: Array<{ latitude: number; longitude: number }> = [];
   let index = 0;
   let lat = 0;
-  let lng = 0;..
+  let lng = 0;
 
   while (index < encoded.length) {
     let result = 0;
